e3ab560494b9d0aca84e63f8c4139ac60d325b07
A1doranM <vaselisk1997@gmail.com>
2024-05-07 09:35:29 +0300

    Quiz App

---
+++ projects/quiz_app/lib/questions_summary.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  final List<Map<String, Object>> summaryData;
+
+  const QuestionsSummary({super.key, required this.summaryData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summaryData.map((data) {
+        return Row(
+          children: [
+            Text(((data['question'] as int) + 1) as String),
+          ],
+        );
+      }).toList(),
+    );
+  }
+}
